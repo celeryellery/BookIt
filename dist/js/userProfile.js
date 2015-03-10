5660776313d5ec676a6f1b70771748743923765a
Ellery <elleryw@uw.edu>
2015-03-09 19:19:36 -0700

    Added modals to change username and password

--- dist/js/userProfile.js
+++ dist/js/userProfile.js
@@ -29,3 +29,49 @@ function logout()
 	app.sessionDatabase.write(user);
 	window.location = "SignIn.html";
 }
+
+
+//After entering in username change information, the new username is submitted to the database
+function submitUsername() {
+	var currentUsername = user.email;
+	var newUsername = document.getElementById("newUsername").value;
+	var newUsernameConfirm = document.getElementById("newUsernameConfirm").value;
+
+	// TODO: Add error checking to see if current username isn't entered correctly, 
+	// or if new username doesn't match confirmed new username
+	
+	user.email = newUsername;
+	
+	clearAllFormData();
+	$('#usernameModal').modal('hide');
+	if (isSupported()) {
+		var db = app.database.read();
+		db.push(book);
+		app.database.write(db);
+		$('#confirm').show();
+	} else {
+		$('#rejected').show();
+	}
+}
+
+//After entering in password change information, the new password is submitted to the database
+function submitPassword() {
+	var currentPassword = user.password;
+	var newPassword = document.getElementById("newPassword").value;
+	var newPasswordConfirm = document.getElementById("newPasswordConfirm").value;
+
+		// TODO: Add error checking to see if current password isn't entered correctly, 
+	// or if new password doesn't match confirmed new password
+	user.password = newPassword;
+	
+	clearAllFormData();
+	$('#passwordModal').modal('hide');
+	if (isSupported()) {
+		var db = app.database.read();
+		db.push(book);
+		app.database.write(db);
+		$('#confirm').show();
+	} else {
+		$('#rejected').show();
+	}
+}
